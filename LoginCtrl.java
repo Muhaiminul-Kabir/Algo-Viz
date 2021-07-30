fa56b822c43fcea7885aefbb2dc01b1e392ee927
Muhaiminul-Kabir <muhaiminulkabir32@gmail.com>
2021-07-30 17:04:43 +0600

    Login, SignUp , DashBoard initialized

---
+++ LoginCtrl.java
@@ -0,0 +1,72 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package study;
+
+import java.io.IOException;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Node;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.control.TextField;
+import javafx.stage.Stage;
+
+/**
+ *
+ * @author ASUS
+ */
+public class LoginCtrl {
+    @FXML 
+    Button loginB;
+    @FXML 
+    TextField userIn;
+    
+    
+    private Stage stage;
+    private Scene scene;
+    private Parent root;
+
+    String userName = userIn.getText();
+    
+    
+    
+    public void login(ActionEvent event) throws IOException {
+        closeWindowOnButton(loginB);
+        dashInit(event);
+    }
+
+    public void signUP(ActionEvent event) throws IOException {
+
+        Stage stage = new Stage();
+        Parent root = FXMLLoader.load(getClass().getResource("/study/signUpPage.fxml"));
+        Scene scene = new Scene(root);
+        stage.setScene(scene);
+        stage.show();
+
+    }
+
+    public void dashInit(ActionEvent event) throws IOException {
+        FXMLLoader loader = new FXMLLoader(getClass().getResource("/study/dashBoard.fxml"));
+        root = loader.load();
+
+        DashBoardCtrl myDash = loader.getController();
+        //	myDash.displayName(username);
+
+        stage = (Stage) ((Node) event.getSource()).getScene().getWindow();
+        scene = new Scene(root);
+        stage.setScene(scene);
+        stage.show();
+    }
+    
+    public void closeWindowOnButton(Button b) {
+        // get a handle to the stage
+        Stage stage = (Stage) b.getScene().getWindow();
+        // do what you have to do
+        stage.close();
+    }
+}
