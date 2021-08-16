7caad8c9395a244aa90471f872da85ae3cd3f45e
Muhaiminul-Kabir <muhaiminulkabir32@gmail.com>
2021-08-17 02:34:37 +0600

    window related fxml made

--- studycmp/MAINMENUController.java
+++ studycmp/MAINMENUController.java
@@ -6,11 +6,16 @@
 package studycmp;
 
 import com.jfoenix.controls.JFXButton;
+import java.io.IOException;
 import java.net.URL;
 import java.util.ResourceBundle;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
+import javafx.scene.layout.Pane;
 
 /**
  * FXML Controller class
@@ -31,37 +36,66 @@ public class MAINMENUController implements Initializable {
     private JFXButton settingsButton;
     @FXML
     private JFXButton progressButton;
+    @FXML
+    private Pane windowContainer;
+    
+    
+    public Pane tempPane;
+    private String[] window = {"/studycmp/HOME.fxml","/studycmp/STUDY.fxml","/studycmp/TODO.fxml","/studycmp/NOTES.fxml","/studycmp/SETTINGS.fxml","/studycmp/PROGRESS.fxml"};
 
     /**
      * Initializes the controller class.
      */
     @Override
     public void initialize(URL url, ResourceBundle rb) {
-        // TODO
+        try {
+            loadWindow(0);
+        } catch (IOException ex) {
+            System.out.println(ex);
+        }
     }    
 
     @FXML
-    private void loadHome(ActionEvent event) {
+    private void loadHome(ActionEvent event) throws IOException {
+        loadWindow(0);
     }
 
     @FXML
-    private void loadStudy(ActionEvent event) {
+    private void loadStudy(ActionEvent event) throws IOException {
+        loadWindow(1);
     }
 
     @FXML
-    private void loadTasks(ActionEvent event) {
+    private void loadTasks(ActionEvent event) throws IOException {
+        loadWindow(2);
     }
 
     @FXML
-    private void loadNotes(ActionEvent event) {
+    private void loadNotes(ActionEvent event) throws IOException {
+        loadWindow(3);
     }
 
     @FXML
-    private void loadSettings(ActionEvent event) {
+    private void loadSettings(ActionEvent event) throws IOException {
+        loadWindow(4);
     }
 
     @FXML
-    private void loadProgress(ActionEvent event) {
+    private void loadProgress(ActionEvent event) throws IOException {
+        loadWindow(5);
     }
     
+    
+    public void loadWindow(int index) throws IOException{
+        FXMLLoader loader = new FXMLLoader(getClass().getResource(window[index]));
+        Pane newLoadedPane = loader.load();
+        windowContainer.getChildren().remove(tempPane);
+
+        windowContainer.getChildren().add(newLoadedPane);
+
+        tempPane = newLoadedPane;
+
+    
+    
+    }
 }
