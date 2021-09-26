d4d9f5ec865cd1a4d4158b0f72672ce2dfc98dac
rafidreezwan <rafidreezwan@gmail.com>
2021-09-27 01:19:44 +0600

    msg

--- studycmp/STUDYTOPICController.java
+++ studycmp/STUDYTOPICController.java
@@ -7,12 +7,17 @@ package studycmp;
 
 import com.jfoenix.controls.JFXButton;
 import com.jfoenix.controls.JFXProgressBar;
+import java.io.IOException;
 import java.net.URL;
 import java.util.ResourceBundle;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
 import javafx.scene.control.Label;
+import javafx.stage.Stage;
 
 /**
  * FXML Controller class
@@ -41,15 +46,34 @@ public class STUDYTOPICController implements Initializable {
     }    
 
     @FXML
-    private void startSession(ActionEvent event) {
+    private void startSession(ActionEvent event) throws Exception {
+        fakeBar.setVisible(true);
+        realBar.setVisible(false);
+        loadTimer();
     }
 
     @FXML
     private void delTopic(ActionEvent event) {
     }
 
-    void setData(String string) {
+    void setData(String string) throws IOException {
         topicName.setText(string);
+        API.overwriteFile("src/StudyBase/curr_std.txt", string);
+    }
+
+    private void loadTimer() throws IOException, Exception {
+      System.out.println("psg");
+                    FXMLLoader loader = new FXMLLoader(
+                            getClass().getResource(
+                                    "/studycmp/CLOCK.fxml"
+                            ));
+                    Parent root = loader.load();
+                    CLOCKController ctrl = loader.getController();
+                    ctrl.setBool();
+                    Scene scene = new Scene(root);
+                    Stage stage = new Stage();
+                    stage.setScene(scene);
+                    stage.show();
     }
     
 }
